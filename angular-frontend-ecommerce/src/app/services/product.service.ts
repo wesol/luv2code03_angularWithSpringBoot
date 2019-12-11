eaa3fb24c59f0398d858b138d5e9bc011e11ddea
Mateusz Wesołowski <mateusz.wesolowski@eyzee.com>
2019-12-11 12:45:15 +0100

    Angular component updated to subscribe

--- angular-frontend-ecommerce/src/app/services/product.service.ts
+++ angular-frontend-ecommerce/src/app/services/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getProductList(): Observable<Product[]>{
+  getProductList(): Observable<Product[]> {
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products));
   }
 }
